65ee60d05d81044d6fb5ed969c550de2e087d9f8
the-tranquillity <katyanov@protonmail.com>
2022-11-19 02:54:22 +0800

    forgot to add icon styles, fixed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
