a6d44f2f034d819753776f72092efe423833d486
the-tranquillity <katyanov@protonmail.com>
2022-11-19 02:12:58 +0800

    users teaser added, home card setup

--- src/app/pages/mainPage.jsx
+++ src/app/pages/mainPage.jsx
@@ -1,28 +1,31 @@
-import Member from "../components/ui/member";
-import members from "../mockData/users.json";
+import Member from '../components/ui/member';
+import members from '../mockData/users.json';
 
 const Main = () => {
-    return (
-        <div>
-            <h1>О нашей команде</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consequatur
-                culpa cum cumque debitis earum eos eum ex exercitationem fugiat, fugit iure magni
-                minima molestiae, neque nobis non praesentium quae quibusdam quod rem sit sunt
-                suscipit temporibus velit veniam, vitae voluptatum. Aliquam at blanditiis debitis
-                deleniti distinctio, ea exercitationem ipsam, iure magni nemo nihil, qui repudiandae
-                similique sint soluta. Adipisci alias animi aspernatur dolor, doloremque ipsum
-                laboriosam maiores optio quibusdam, reiciendis sapiente vero voluptas. Adipisci
-                magni minima repellat reprehenderit vero! Eligendi, modi, nesciunt? A aliquam
-                corporis cum delectus dignissimos, harum inventore labore nesciunt non repellendus
-                sit soluta totam veniam voluptates.
-            </p>
-            <h2>Участники</h2>
-            {members.map((m) => (
-                <Member key={m._id} user={m} />
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      <h1>О нашей команде</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        consequatur culpa cum cumque debitis earum eos eum ex exercitationem
+        fugiat, fugit iure magni minima molestiae, neque nobis non praesentium
+        quae quibusdam quod rem sit sunt suscipit temporibus velit veniam, vitae
+        voluptatum. Aliquam at blanditiis debitis deleniti distinctio, ea
+        exercitationem ipsam, iure magni nemo nihil, qui repudiandae similique
+        sint soluta. Adipisci alias animi aspernatur dolor, doloremque ipsum
+        laboriosam maiores optio quibusdam, reiciendis sapiente vero voluptas.
+        Adipisci magni minima repellat reprehenderit vero! Eligendi, modi,
+        nesciunt? A aliquam corporis cum delectus dignissimos, harum inventore
+        labore nesciunt non repellendus sit soluta totam veniam voluptates.
+      </p>
+      <h2>Участники</h2>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        {members.map((m) => (
+          <Member key={m._id} user={m} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Main;
