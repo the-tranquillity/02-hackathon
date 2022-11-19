986c9fdd9226a726026d6730c79081c41071f688
the-tranquillity <katyanov@protonmail.com>
2022-11-19 10:32:09 +0800

    pages basic version

--- src/app/pages/favourites.jsx
+++ src/app/pages/favourites.jsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+import { fromStorage } from "../utils/fromStorage";
+import { toStorage } from "../utils/toStorage";
+import { MATES_STORAGE } from "../constants/constants";
+import teamMockData from "../mockData/teamMates.json";
+import TeamMateCard from "../components/ui/teamMateCard";
+import { Link } from "react-router-dom";
+
 const Favourites = () => {
-  return <div>favourites</div>;
+    const [teamMates, setTeamMates] = useState(fromStorage(MATES_STORAGE) || teamMockData);
+    useEffect(() => {
+        if (!fromStorage(MATES_STORAGE)) {
+            toStorage(MATES_STORAGE, teamMates);
+        }
+    }, [teamMates]);
+    const favMates = teamMates.filter((m) => m.isFavourite);
+    return (
+        <div>
+            <h1>Избранное</h1>
+            <div className="row row-cols-1 row-cols-md-2 g-4 mt-2">
+                {favMates.length
+? (
+                    favMates.map((m) => (
+                        <TeamMateCard key={m._id} mate={m} setTeamMates={setTeamMates} />
+                    ))
+                )
+: (
+                    <div className="col">
+                        <p>В избранном пока никого нет.</p>
+                        <Link type="button" className="btn btn-primary" to="/">
+                            Посмотреть участников команды
+                        </Link>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Favourites;
