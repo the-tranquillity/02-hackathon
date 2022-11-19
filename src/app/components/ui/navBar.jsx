f3813286fefa40d1d4438c1ca8bba61453650330
the-tranquillity <katyanov@protonmail.com>
2022-11-19 08:11:42 +0800

    rem app.js, lint navbar

--- src/app/components/ui/navBar.jsx
+++ src/app/components/ui/navBar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { NavLink, Link } from "react-router-dom";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     const [isOpen, setOpen] = useState(false);
